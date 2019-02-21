82760649229291dfb06c49d7625d9eda03bdc8b7
leaf-junkie <ragsdale.jar@gmail.com>
2019-02-21 17:02:20 -0500

    Obtained Spotify client ID and secret

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.SPOTIFY_ID, // 2b96c138c3a4479c9c08526c57a528ec
+  secret: process.env.SPOTIFY_SECRET // 44f3e74f9af64cf3bcdee0851598b5f0
 };
--- liri.js
+++ liri.js
@@ -3,7 +3,33 @@ import keys from "./keys.js";
 var spotify = new Spotify(keys.spotify);
 
 // Commands: 
-//      concert-this
-//      spotify-this-song
-//      movie-this
-//      do-what-it-says
+
+// 1. concert-this
+// This will search the Bands in Town Artist Events API 
+// ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") 
+// for an artist and render the following information about each event to the terminal:
+//      Name of the venue
+//      Venue location
+//      Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+// 2. spotify-this-song
+// This will show the following info about the song in your terminal/bash window
+//      Artist(s)
+//      The song's name
+//      A preview link of the song from Spotify
+//      The album that the song is from
+
+// 3. movie-this
+// This will show the following info in your terminal/bash window:
+//      Title of the movie.
+//      Year the movie came out.
+//      IMDB Rating of the movie.
+//      Rotten Tomatoes Rating of the movie.
+//      Country where the movie was produced.
+//      Language of the movie.
+//      Plot of the movie.
+//      Actors in the movie.
+
+// 4. do-what-it-says
+// Using the fs Node package, LIRI will take the text inside of random.txt
+// and then use it to call one of LIRI's commands.
