cec6fbba1c584a45b29ad017e7594d7ac86f4172
leaf-junkie <ragsdale.jar@gmail.com>
2019-02-21 17:17:24 -0500

    Added functions as placeholders for the structure of liri.js

--- liri.js
+++ liri.js
@@ -1,8 +1,18 @@
+// Require statement and variables
+
 require("dotenv").config();
+
 import keys from "./keys.js";
+
 var spotify = new Spotify(keys.spotify);
 
-// Commands: 
+// user input variables
+const liri_command = process.argv[2];
+const liri_search = process.argv[3];
+console.log("LIRI command: " + liri_command);
+console.log("LIRI search: " + liri_search);
+
+// LIRI functions
 
 // 1. concert-this
 // This will search the Bands in Town Artist Events API 
@@ -12,12 +22,19 @@ var spotify = new Spotify(keys.spotify);
 //      Venue location
 //      Date of the Event (use moment to format this as "MM/DD/YYYY")
 
+function liriConcertThis() {
+    
+}
+
 // 2. spotify-this-song
 // This will show the following info about the song in your terminal/bash window
 //      Artist(s)
 //      The song's name
 //      A preview link of the song from Spotify
 //      The album that the song is from
+function liriSpotifyThisSong() {
+
+}
 
 // 3. movie-this
 // This will show the following info in your terminal/bash window:
@@ -29,7 +46,13 @@ var spotify = new Spotify(keys.spotify);
 //      Language of the movie.
 //      Plot of the movie.
 //      Actors in the movie.
+function liriMovieThis() {
+
+}
 
 // 4. do-what-it-says
 // Using the fs Node package, LIRI will take the text inside of random.txt
 // and then use it to call one of LIRI's commands.
+function liriDoWhatItSays() {
+
+}
