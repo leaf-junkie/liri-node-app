5e30fda797f4de597b428b5151be2c92f2532007
leaf-junkie <ragsdale.jar@gmail.com>
2019-02-24 17:57:21 -0500

    adding more to functions

--- liri.js
+++ liri.js
@@ -1,29 +1,24 @@
 // Require statement and variables
-
 require("dotenv").config();
-// import * as dotenv from "dotensv";
-// dotenv.config()
-import keys from "./keys.js";
-import { readFile } from "fs";
-
+const axios = require("axios");
+const fs = require("fs");
+const keys = require("./keys.js"); 
+const moment = require("moment");
+moment().format();
+const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
 
 // user input variables
-const liri_command = process.argv[2];
-const liri_search = process.argv[3];
+const command = process.argv[2];
+const search = process.argv.slice(3).join("");
 console.log("LIRI command: " + liri_command);
 console.log("LIRI search: " + liri_search);
-
 
 // LIRI functions
 
 // 1. concert-this
 // Search the Bands in Town Artist Events API for an artist and render venue name, location, and date for each event
 function concertThis() {
-    // Grab the axios package
-    const axios = require("axios");
-    
-    // Run the axios.get function (this is like making an ajax call)
     axios
         .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
         .then(function(response) {
@@ -44,45 +39,51 @@ function concertThis() {
         });
 
     // TODO: Clean up code, especially console logs
-    // a. Name of the venue
+    const artistName = artist.lineup[0];
     const venueName = artist.venue.name;
-    console.log(`Venue: ${venueName}`);
-
-    // b. Venue location
     const venueCity = artist.venue.city;
     const venueState = artist.venue.region;
-    console.log(`Location: ${venueCity}, ${venueState}`);
-
-    // c. Date of the Event (use moment to format this as "MM/DD/YYYY")
     const eventDate = artist.datetime;
-    console.log(`Date: ${date}`);
-    // TODO: Use moment.js to format the date
+    
+    console.log(`Artist: ${artistName}`);
+    console.log(`Venue: ${venueName}`);
+    console.log(`Location: ${venueCity}, ${venueState}`);
+    console.log(`Date: ${eventDate}`);
+    // TODO: Use moment.js to format the date ("MM/DD/YYYY")
 }
-concertThis();
 
 // 2. spotify-this-song
-// TODO: Unsure if axios is necessary - need to use keys.js for the API string!!
-// This will show the following info about the song in your terminal/bash window
 function spotifyThisSong() {
-    const axios = require("axios");
-    axios
-        .get("")
-        .then(function(response) {
-            console.log(response.data);
-        })
-        .catch(function(error) {
-            if (error.response) {
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log("Error:", error.message);
-            }
-            console.log(error.config);
+    if(!search) {
+        search = "heimdalsgate like a promethean curse";
+        console.log("Showing results for Heimdalsgate Like a Promethean Curse by of Montreal");
+    }
+    spotify.search({
+        type: "track",
+        query: search,
+        limit: 5
+    }, function(err, data) {
+        if(err) {
+            console.log(`Oops, we encountered an error: ${err}`);
+        } else {
+            
+        }
+    })
+    
+    .catch(function(error) {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log("Error:", error.message);
+        }
+        console.log(error.config);
         });
 
+    // TODO: chec Sportify API JSON structure
     // a. Artist(s)
 
     // b. The song's name
@@ -92,12 +93,13 @@ function spotifyThisSong() {
     // d. The album that the song is from
 
 }
-spotifyThisSong();
 
 // 3. movie-this
 // This will show the following info in your terminal/bash window:
 function movieThis() {
-    const URL = ; // TODO: Add URL
+    const search = process.argv.slice(3).join(""); // TODO: variable name
+
+    const URL = "" + search; // TODO: Add URL
     axios.get(URL).then(function(response) {
 
         const movieData = response.data[].; // TODO: Look up API JSON structure
@@ -128,7 +130,6 @@ function movieThis() {
     console.log(movieObject);
 })
 }
-movieThis()
 
 // 4. do-what-it-says
 function doWhatItSays() {
@@ -137,9 +138,21 @@ function doWhatItSays() {
     });
     // console.log(`Now playing ${song} by ${artist}`);
 }
-doWhatItSays();
 
 // Switch statement
-
+switch (function) {
+    case "":
+        console.log();
+        break;
+    case "":
+        console.log();
+        break;
+    case "":
+        console.log();
+        break;
+    case "":
+        console.log();
+        break;
+};
 
 module.exports
