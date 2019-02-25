c0c002f5a1c0a1647589e6c12c33373503fe87b1
leaf-junkie <ragsdale.jar@gmail.com>
2019-02-25 00:15:22 -0500

    functions are working great

--- liri.js
+++ liri.js
@@ -12,7 +12,7 @@ const divider = "\n\n....................\n\n"
 
 // user input variables
 const command = process.argv[2];
-const search = process.argv.slice(3).join("+");
+let search = process.argv.slice(3).join("+");
 console.log(search);
 
 // Switch statement
@@ -29,26 +29,19 @@ switch (command) {
     case "do-what-it-says":
         doWhatItSays();
         break;
-    // default:
 };
 
 // 1. concert-this
 async function concert() {
     const URL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
-    // console.log(URL);
     const response = await axios.get(URL);
     const result = response.data[0];
-    // console.log(result.length);
-    // console.log(`Response: ${util.inspect(result)}`);
-
     const lineup = result.lineup;
-    console.log(lineup.join(", "));
     const venue = result.venue.name;
     const city = result.venue.city;
     const state = result.venue.region;
-    const date = result.datetime;
-    // TODO: Use moment.js to format the date ("MM/DD/YYYY")
-
+    const date = moment(result.datetime).format("MM/DD/YYYY");
+    
     const concertData = [
         `Lineup: ${lineup.join(", ")}`,
         `Venue: ${venue}`,
@@ -65,92 +58,59 @@ async function concert() {
 
 // 2. spotify-this-song
 function song() {
+    console.log('hello');
     spotify.search({
         type: "track",
         query: search,
         limit: 5
     }, function(err, data) {
+        console.log(data)
         if(!search) {
             search = "heimdalsgate like a promethean curse";
             console.log("Showing results for Heimdalsgate Like a Promethean Curse by of Montreal");
-        }
-        
-        if(err) {
-            console.log(`Oops, we encountered an error: ${err}`);
         } else {
-            for(a = 0; a < limit.length; a++) {
-                const results = data.tracks.items[a];
+            for(i = 0; i < data.tracks.items.length; i++) {
+                const results = data.tracks.items[i];
                 const songData = [
-                    `\n\n....................\n\n`,
-                    `Song Name: ${results.name}`,
+                    `Song: ${results.name}`,
                     `Artist: ${results.artists[0].name}`,
                     `Album: ${results.album.name}`,
-                    `Preview: ${results.preview_url}`,
-                    `\n\n....................\n\n`
+                    `Preview: ${results.preview_url}`
                 ];
-                console.log(songData);
+                console.log(divider + songData.join("\n") + divider);
             }
         }
     });
 }
 
-// TODO: Format printed movieData
 // 3. movie-this
-function movie() {
-    axios
-        .get("http://www.omdbapi.com/?t=" + search + "&apikey=3bcc87a3")
-        .then(function(response) {
-        const result = response.data;
-        const movieData = [
-            `Title: ${result.Title}`,
-            `Year Released: ${result.Year}`,
-            `IMDB Rating: ${result.Ratings[0].Value}`,
-            `Rotten Tomatoes Rating: ${result.Ratings[1].Value}`,
-            `Country Produced: ${result.Country}`,
-            `Language: ${result.Language}`,
-            `Plot: ${result.Plot}`,
-            `Actors: ${result.Actors}`,
+async function movie() {
+    if (!search) {
+        search = "Donnie+Darko";
+    }
+    const response = await axios.get("http://www.omdbapi.com/?t=" + search + "&apikey=3bcc87a3")
+    const result = response.data;
+    const movieData = [
+        `Title: ${result.Title}`,
+        `Year: ${result.Year}`,
+        `IMDB Rating: ${result.Ratings[0].Value}`,
+        `Rotten Tomatoes: ${result.Ratings[1].Value}`,
+        `Country: ${result.Country}`,
+        `Language: ${result.Language}`,
+        `Actors: ${result.Actors}`,
+        `Plot: ${result.Plot}`,
     ];
-    
-    fs.appendFile("log.txt", movieData, (err) => {
-        if (err) throw err;
+    const displayString = divider + movieData.join("\n") + divider;
+    fs.appendFile("log.txt", displayString, (err) => {
+            console.log(displayString);
     });
-
-    console.log(divider + movieData + divider);
-})
 }
-
-// async function movieThis() {
-//     const response = await axios.get("http://www.omdbapi.com/?t=" + search + "&apikey=3bcc87a3");
-//     const result = response.data;
-//     const movieData = [
-//         `Title: ${result.Title}`,
-//         `Year Released: ${result.Year}`,
-//         `IMDB Rating: ${result.Ratings[0].Value}`,
-//         `Rotten Tomatoes Rating: ${result.Ratings[1].Value}`,
-//         `Country Produced: ${result.Country}`,
-//         `Language: ${result.Language}`,
-//         `Plot: ${result.Plot}`,
-//         `Actors: ${result.Actors}`,
-//     ];
-
-//     fs.appendFile("log.txt", movieData, (err) => {
-//         if (err) throw err;
-//     });
-
-//     console.log(divider + movieData + divider);
-// }
-
 
 // 4. do-what-it-says
 function doWhatItSays() {
     const data = fs.readFileSync("random.txt").toString();
     if(data) {
-        console.log(
-            `\n\n....................\n\n`,
-            `${data}`,
-            `\n\n....................\n\n`
-        );
+        console.log(divider + data + divider);
     }
 }
 
